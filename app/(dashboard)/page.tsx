ded71d66b79820fcf1ef0f70b9529f3c74962329
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2023-12-09 23:37:06 +0530

    create collection done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,37 +1,87 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
+import { prisma } from "@/lib/prisma";
 import { wait } from "@/lib/wait";
 import { currentUser } from "@clerk/nextjs";
 import { Suspense } from "react";
+import SadFace from "@/components/icons/SadFace";
+import CreateCollectionBtn from "@/components/CreateCollectionBtn";
+import CollectionCard from "@/components/CollectionCard";
 
 export default async function Home() {
   return (
-    <Suspense fallback={<WelcomeMssgFallback/>}>
-      <WelcomeMssg />
-    </Suspense>
-  )
+    <>
+      <Suspense fallback={<WelcomeMsgFallback />}>
+        <WelcomMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading collections...</div>}>
+        <CollectionList />
+      </Suspense>
+    </>
+  );
+}
 
-  async function WelcomeMssg() {
-    const user = await currentUser();
-    await wait(3000);
-    if (!user) {
-      return <div>sheeeeeshhh big error</div>
-    }
-    
-    return(
-      <div className="flex w-full">
-        <h1 className="text-4xl font-bold">HELLO THERE, <br /> {user.firstName} {user.lastName}</h1>
+async function WelcomMsg() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <div>error</div>;
+  }
+
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        Welcome, <br /> {user.firstName} {user.lastName}
+      </h1>
+    </div>
+  );
+}
+
+function WelcomeMsgFallback() {
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        <Skeleton className="w-[180px] h-[36px]" />
+        <Skeleton className="w-[150px] h-[36px]" />
+      </h1>
+    </div>
+  );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    include: {
+      tasks: true,
+    },
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          {/*<SadFace />*/}
+          <AlertTitle className="text-2xl">There are no collections yet!</AlertTitle>
+          <AlertDescription className="text-xl">
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
       </div>
     );
   }
 
-  function WelcomeMssgFallback() {
-    return(
-      <div className="flex w-full">
-        <div className="text-4xl font-bold">
-          <Skeleton className="w-[150px] h-[36px]" />
-          <Skeleton className="w-[150px] h-[36px]" />
-        </div>
+  return (
+    <>
+      <CreateCollectionBtn />
+      <div className="flex flex-col gap-4 mt-6">
+        {collections.map((collection) => (
+          <CollectionCard key={collection.id} collection={collection} />
+        ))}
       </div>
-    );
-  }
+    </>
+  );
 }
